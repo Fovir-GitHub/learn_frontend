7ec040c640bc054084e10fe8c46b38f7738386f0
Fovir <fovir@disroot.org>
2025-08-06 16:34:14 +0800

    feat: Learn variables in JavaScript.

---
+++ javascript/variables/main.js
@@ -0,0 +1,15 @@
+let arr = ["First", "Second", "Third"];
+console.log(arr);
+
+let dog = {
+  name: "Spot",
+  breed: "Dalmatian",
+};
+
+console.log(dog);
+console.log(dog.name);
+
+let myNumber = "500";
+console.log(typeof myNumber);
+myNumber = 500;
+console.log(typeof myNumber);
