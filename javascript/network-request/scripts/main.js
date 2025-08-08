d2c677b95d27f77727e41ef41a1324cb45adb4ac
Fovir <fovir@disroot.org>
2025-08-08 14:31:47 +0800

    feat: Learn network request.

---
+++ javascript/network-request/scripts/main.js
@@ -0,0 +1,31 @@
+const verseChoose = document.querySelector("select");
+const poemDisplay = document.querySelector("pre");
+
+verseChoose.addEventListener("change", () => {
+  const verse = verseChoose.value;
+  updateDisplay(verse);
+});
+
+/**
+ * @param {string} verse
+ */
+function updateDisplay(verse) {
+  verse = verse.replace(" ", "").toLowerCase();
+  const url = `${verse}.txt`;
+
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then((text) => {
+      poemDisplay.textContent = text;
+    })
+    .catch((error) => {
+      poemDisplay.textContent = `Could not fetch verse: ${error}`;
+    });
+}
+
+updateDisplay("Verse 1");
