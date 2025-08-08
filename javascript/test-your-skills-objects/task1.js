35db5e53200f198841dac0bf3ba5adba3ca7ba8c
Fovir <fovir@disroot.org>
2025-08-08 12:40:12 +0800

    feat: Finish task 1.

---
+++ javascript/test-your-skills-objects/task1.js
@@ -0,0 +1,14 @@
+const cat = {
+  name: "Bertie",
+  breed: "Cymric",
+  color: "white",
+  greeting: function () {
+    console.log("Meow!");
+  },
+};
+
+const catName = cat["name"];
+cat.greeting();
+cat.color = "black";
+
+console.log(cat);
