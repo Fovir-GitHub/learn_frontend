977d2b3240546a27927dd76656210b9d11b1cd65
Fovir <fovir@disroot.org>
2025-08-08 15:56:10 +0800

    chore: Initial commit.

---
+++ react/moz-todo-react/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "DevShell with Node.js.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs
+      ];
+
+      shellHook = ''
+        export NODE_ENV=development
+
+        echo "Entering the development environment!"
+        echo "Node: $(node -v), npm: $(npm -v)"
+
+        trap 'echo "Leaving the development environment!"' EXIT
+      '';
+    };
+  };
+}
