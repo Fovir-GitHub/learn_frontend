21bd721bed3937735dbd8b54b86834044b8b1014
Fovir <fovir@disroot.org>
2025-08-16 15:35:11 +0800

    feat: Add `toggleTaskCompleted`.

--- react/moz-todo-react/src/App.jsx
+++ react/moz-todo-react/src/App.jsx
@@ -7,12 +7,24 @@ import { nanoid } from "nanoid";
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
 
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+
+    setTasks(updatedTasks);
+  }
+
   const taskList = tasks?.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
     />
   ));
 
