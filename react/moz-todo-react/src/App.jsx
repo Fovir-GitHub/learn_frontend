44547915bd8404ffcce2cae8c0b567efe6978977
Fovir <fovir@disroot.org>
2025-08-16 15:26:58 +0800

    feat: Add feature of `addTask()`;

--- react/moz-todo-react/src/App.jsx
+++ react/moz-todo-react/src/App.jsx
@@ -1,16 +1,29 @@
 import Todo from "./components/Todo";
 import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
+import { useActionState, useState } from "react";
 
 function App(props) {
-  const taskList = props.tasks?.map((task) => (
-    <Todo id={task.id} name={task.name} completed={task.completed} />
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const taskList = tasks?.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
   ));
+
+  function addTask(name) {
+    const newTask = { id: "id", name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
